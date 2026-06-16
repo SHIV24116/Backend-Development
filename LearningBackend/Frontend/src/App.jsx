f3ed786c59d6befdd4acfc397d6b72402b2f5e36
SHIV24116 <singhshivendra1945@gmail.com>
2026-06-16 19:30:55 +0530

    updated

---
+++ LearningBackend/Frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import axios from 'axios'
+import { useEffect } from "react"
+
+
+function App() {
+  const [jokes,setjokes] =useState([])
+
+  useEffect(()=>{
+    axios.get('/api/jokes')
+    .then((response)=>{
+      setjokes(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  })
+
+  return (
+    <>
+      <h1>Hi, this is the frontend of my Project</h1>
+      <p>JOKES : {jokes.length}</p>
+      {
+        jokes.map((joke,index)=>(
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+export default App
