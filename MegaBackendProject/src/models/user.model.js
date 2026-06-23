3172167816c4a5144c6a237b38d6dca09804473f
SHIV24116 <singhshivendra1945@gmail.com>
2026-06-23 22:53:25 +0530

    updated

---
+++ MegaBackendProject/src/models/user.model.js
@@ -0,0 +1,84 @@
+import mongoose,{Schema} from "mongoose"
+
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema=new Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim: true,
+            index:true,  //specially in mongodb if we want to implement searching....doing indexing is good        
+        },
+        fullname:{
+            type:String,
+            required:true,
+            trim: true,
+            index:true,  //specially in mongodb if we want to implement searching....doing indexing is good        
+        },
+        avatar:{
+            type:String,  //cloudinary se url use karenge
+            required:true,
+        },
+        coverImage:{
+            type:String
+        },
+        watchHistory:[
+            {
+               type:Schema.Types.ObjectId,
+               ref:"Video"
+            }
+        ],
+        password:{
+            type:String,
+            required:[true,'password is required']        
+        },
+        refreshToken:{   //what are tockens??
+            type: String  
+        }
+    },{timestamps:true}
+)
+
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect =async function(password){
+    return await bcrypt.compare(password,this.password)   //here this.password is encrypted password
+}
+
+userSchema.methods.generateAccessToken =function(){
+    return jwt.sign(
+        {                   //this is the payload....whatever parameters we want to pass
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken =function(){
+    return jwt.sign(
+        {                   //this is the payload....whatever parameters we want to pass
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+export const User=mongoose.model("User",userSchema)
--- MegaBackendProject/src/models/video.model.js
+++ MegaBackendProject/src/models/video.model.js
@@ -1,4 +1,6 @@
-import mongoose,{Schema} from "mongoose"
+import mongoose,{Schema} from "mongoose";
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema=new Schema(
     {
@@ -32,5 +34,8 @@ const videoSchema=new Schema(
         }
     },{timestamps:true}
 )
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
 
 export const Video=mongoose.model("Video",videoSchema)
